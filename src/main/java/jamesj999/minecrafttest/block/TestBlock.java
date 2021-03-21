931c5c1a50132618ef17c52cd49002c4ae465da9
jamesj999 <jamesj999@gmail.com>
2021-03-21 18:30:37 +0000

    Added test Event handling logic to TestBlock (TestBlockInteractEventHandler does not yet work...)

--- src/main/java/jamesj999/minecrafttest/block/TestBlock.java
+++ src/main/java/jamesj999/minecrafttest/block/TestBlock.java
@@ -3,14 +3,54 @@ package jamesj999.minecrafttest.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TestBlock extends Block {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public TestBlock(Properties properties) {
         super(properties);
+        MinecraftForge.EVENT_BUS.register(new TestBlockBrokenEventHandler());
     }
 
     public static Properties generateProperties() {
         Properties properties = Properties.create(Material.ROCK, MaterialColor.STONE);
+        properties.setRequiresTool();
         return properties;
+    }
+
+    public class TestBlockBrokenEventHandler {
+        @SubscribeEvent
+        public void breakBlock(BlockEvent.BreakEvent breakEvent) {
+            if (breakEvent.getState().getBlock() instanceof TestBlock) {
+                breakEvent.getPlayer().sendMessage(new StringTextComponent("Test Block broken by " + breakEvent.getPlayer().getScoreboardName() + " at " + breakEvent.getPos()), breakEvent.getPlayer().getUniqueID());
+            }
+        }
+    }
+
+    public class TestBlockInteractEventHandler {
+        @SubscribeEvent
+        public void blockInteract(BlockEvent.BlockToolInteractEvent interactEvent) {
+            if (interactEvent.getState().getBlock() instanceof TestBlock) {
+                interactEvent.getPlayer().sendMessage(new StringTextComponent("test"), interactEvent.getPlayer().getUniqueID());
+                if (ToolType.HOE.equals(interactEvent.getToolType())) {
+                    interactEvent.getPlayer().sendMessage(new StringTextComponent("Don't HOE me bro"), interactEvent.getPlayer().getUniqueID());
+                    interactEvent.setResult(Event.Result.ALLOW);
+                } else {
+                    interactEvent.getPlayer().applyKnockback(10f, 10d, 10d);
+                    interactEvent.setResult(Event.Result.DENY);
+                }
+            }
+        }
     }
 }
