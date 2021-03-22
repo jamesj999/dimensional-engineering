b3fe4847380a83a08332c6c1a395e406dacb7333
jamesj999 <jamesj999@gmail.com>
2021-03-22 21:04:11 +0000

    Tile Entity example

--- src/main/java/uk/co/dimensionalengineering/tile/PrismaticWorkbenchTileEntity.java
+++ src/main/java/uk/co/dimensionalengineering/tile/PrismaticWorkbenchTileEntity.java
@@ -10,6 +10,9 @@ import uk.co.dimensionalengineering.DimensionalEngineering;
 
 public class PrismaticWorkbenchTileEntity extends TileEntity {
     public static final String ID = "prismatic-workbench";
+    private static final String COUNTER_ID = "counter";
+
+    private int counter = 0;
 
     public PrismaticWorkbenchTileEntity(TileEntityType<?> tileEntityTypeIn) {
         super(tileEntityTypeIn);
@@ -17,11 +20,19 @@ public class PrismaticWorkbenchTileEntity extends TileEntity {
 
     @Override
     public CompoundNBT write(CompoundNBT compound) {
+        compound.putInt(COUNTER_ID, counter);
         return super.write(compound);
     }
 
     @Override
     public void read(BlockState state, CompoundNBT nbt) {
+        counter = nbt.getInt(COUNTER_ID);
         super.read(state, nbt);
+    }
+
+    public int increase() {
+        counter++;
+        markDirty();
+        return counter;
     }
 }
